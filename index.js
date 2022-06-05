901f4171041c3b5174c0172c0d0af6a4c9a99854
nathanguevara <nathan.guevara@gmail.com>
2022-06-05 12:49:08 -0500

    initial commit

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 
 const express = require('express')
+app.use(express.bodyParser())
+app.use(express.json());
 const app = express()
 const port = process.env.PORT || 4000
 
 const { users } = require('./state')
 
-/* BEGIN - create routes here */
+// respond to GET req containing path "/users" by returning users array
+ app.get('/users', (req, res) => {
+   res.json(users)
+ });
 
-
-/* END - create routes here */
+// respond to GET req containing path "/users/1" by returning the first object
+// from users array
+app.get('/users/1', (req, res) => {
+  res.json(users[0]);
 
 app.listen(port, () => 
   console.log(`Example app listening on port ${port}!`))
